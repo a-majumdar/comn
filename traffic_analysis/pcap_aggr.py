fe4c574fb33d16fd79951f20dd03149b8296f870
Ananya Majumdar <43969433+a-majumdar@users.noreply.github.com>
2022-02-11 11:53:33 +0000

    retest

--- traffic_analysis/pcap_aggr.py
+++ traffic_analysis/pcap_aggr.py
@@ -11,38 +11,24 @@ class Node(object):
         self.left = None
         self.right = None
         self.ip = ip
-        
-        
+
     def add(self, ip, plen):
-        """ Function to recursively traverses the tree down to a leaf, then inserts a new entry with the given address and packet length.
-        Args:
-            ip (String): an ipv4 address.
-            plen (float): the packet length.
-        """
-        # If the ip address is equal to current node then
-        if ip == self.ip:
-            # Add the packet length to the bytes of the current node.
-            self.bytes += plen
-        # If the ip address is less than the current node's ip address then.
-        elif ip < self.ip:
-            # If the there is not a left child node then.
+        #
+        # write your code here
+        if ip < self.ip:
             if self.left is None:
-                # Create a new left child node with the ip address and packet length.
                 self.left = Node(ip, plen)
-            # Else call the add function of the left child.
             else:
                 self.left.add(ip, plen)
-        # Else the ip address will be greater than the current node's ip address
-        else:
-            # If the there is not a right child node then.
+        elif self.ip < ip:
             if self.right is None:
-                # Create a new right child node with the ip address and packet length.
                 self.right = Node(ip, plen)
-            # Else call the add function of the right child.
             else:
                 self.right.add(ip, plen)
-                
-                
+        elif self.ip == ip:
+            self.bytes += plen
+        #
+
     def data(self, data):
         if self.left:
             self.left.data(data)
@@ -50,69 +36,45 @@ class Node(object):
             data[ip_network(self.ip)] = self.bytes
         if self.right:
             self.right.data(data)
-            
-            
-    @staticmethod 
+
+    @staticmethod
     def supernet(ip1, ip2):
-        """ Function which computes and returns the common network address and mask.
-        Args:
-            ip1 (String): an ip address which is either an IPv4Address or IPv4Network object.
-            ip2 (String): an ip address which is either an IPv4Address or IPv4Network object.
-        Returns:
-            String: [description]
-        """
-        # Find the network addresses of the ip addresses
+        # arguments are either IPv4Address or IPv4Network
         na1 = ip_network(ip1).network_address
         na2 = ip_network(ip2).network_address
-        # Calculate the number of bits that are the same starting from the lhs of the string.
-        prefixlen = 32-(int(na1)^int(na2)).bit_length()
-        # Find the binary string respsentation of the network address of one of the network addresses.
-        na_binary = ''.join(["{0:08b}".format(int(x)) for x in str(na2).split('.')])
-        # Find the address of the supernet by only keeping the first prefixlen number of bits.
-        supernet_address = ip_address(int(na_binary[:prefixlen].ljust(32, '0'), 2))
-        # Format the ip address correctly with its prefix length.
-        return ip_network("{}/{}".format(supernet_address, prefixlen), strict=False)
-    
-    
+        #
+        # write your code here
+        suffix = (int(na1) ^ int(na2)).bit_length()
+        netmask = 32 - suffix
+        na2_split = str(na2).split('.')
+        binary_network_address = ''.join(["{0:08b}".format(int(x)) for x in na2_split])
+        supernet_address = ip_address(int(binary_network_address[:netmask].ljust(32, '0'), 2))
+        na1 = supernet_address
+        #
+        return ip_network('{}/{}'.format(na1, netmask), strict=False)
+
     def aggr(self, byte_thresh):
-        """ Function to recursively aggregate nodes which have less than the threshold traffic.
-        Args:
-            byte_thresh (float): number of bytes to be aggregated.
-        """
-        # If the current node has a left node then            
-        if self.left is not None:
-            # Call the aggregate function on the left node
+        #
+        # write your code here
+        if self.left:
             self.left.aggr(byte_thresh)
-            # If the number of bytes in the left node is less than the threshold
             if self.left.bytes < byte_thresh:
-                # Add the bytes of the left node to the current node
                 self.bytes += self.left.bytes
-                # Set the number of bytes on the left node to 0
-                self.left.bytes = 0
-                # Calculate the supernet address of the current node's ip address and the left node ip.
                 self.ip = self.supernet(self.ip, self.left.ip)
-                # If the left node is a leaf node (it has no left or right nodes itself) then
+                self.left.bytes = 0
                 if self.left.left is None and self.left.right is None:
-                    # Delete the left node.
                     self.left = None
-        # If the current node has a right node then     
-        if self.right is not None:
-            # If the node has a right node then
+        if self.right:
             self.right.aggr(byte_thresh)
-            # If the number of bytes in the right node is less than the threshold
             if self.right.bytes < byte_thresh:
-                # Add the bytes of the right node to the current node
                 self.bytes += self.right.bytes
-                # Set the number of bytes on the right node to 0
-                self.right.bytes = 0
-                # Calculate the supernet address of the current node's ip address and the right node ip.
                 self.ip = self.supernet(self.ip, self.right.ip)
-                # If the right node is a leaf node (it has no left or right nodes itself) then
-                if self.right.left is None and self.right.right is None:
-                    # Delete the right node.
+                self.right.bytes = 0
+                if self.right.left is None and self.right.left is None:
                     self.right = None
-            
-            
+        #
+
+
 class Data(object):
     def __init__(self, data):
         self.tot_bytes = 0
@@ -135,8 +97,7 @@ class Data(object):
             cnt += 1
         root.aggr(self.tot_bytes * self.aggr_ratio)
         root.data(self.data)
-        
-        
+
     def Plot(self):
         data = {k: v/1000 for k, v in self.data.items()}
         plt.rcParams['font.size'] = 8
@@ -153,12 +114,10 @@ class Data(object):
             self.aggr_ratio * 100, self.tot_bytes * self.aggr_ratio / 1000))
         plt.savefig(sys.argv[1] + '.aggr.pdf', bbox_inches='tight')
         plt.close()
-        
-        
+
     def _Dump(self):
         with open(sys.argv[1] + '.aggr.data', 'w') as f:
             f.write('{}'.format({str(k): v for k, v in self.data.items()}))
-
 
 if __name__ == '__main__':
     d = Data(sys.argv[1])
