7d500ab7efc7614a0cbdc8c4774dac497aa393b7
Ananya Majumdar <43969433+a-majumdar@users.noreply.github.com>
2022-03-21 20:21:30 +0000

    resetting part 1 to original code

--- sliding_window_protocols/Receiver1.py
+++ sliding_window_protocols/Receiver1.py
@@ -8,33 +8,33 @@ def main(args):
     port = int(args[1])
     filename = args[2]
 
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(("0.0.0.0", port))#bind to all interfaces
 
+
+    #open filename for writing to
     f = open(filename, 'wb+')
-    packets = read_socket(port)
-    for packet in packets:
-        # buffer = bytearray(packet)
+    received = sock.recvfrom(packet_length)
 
-        eof = packet[2]
-        payload = packet[3:]
+    while received: # while there is data in the socket, keep recieving it
+        buffer = received[0]#receive data into buffer
+        buffer = bytearray(buffer)#cast data into byte array
+
+        eof = buffer[2]
+        payload = buffer[3:]
 
         if eof == 1:
-            print("File received")
+            print("End of file reached")
             f.write(payload)
             f.close()
             break
 
         f.write(payload)
 
+        #receive next payload
+        received = sock.recvfrom(packet_length)
 
-def read_socket(port):
-    skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    skt.bind(("0.0.0.0", port))
-    packet = skt.recvfrom(packet_length)
-    while packet:
-        print(packet)
-        yield packet
-        packet = skt.recvfrom(packet_length)
-    skt.close()
+    sock.close()
 
 
 if __name__ == "__main__":
