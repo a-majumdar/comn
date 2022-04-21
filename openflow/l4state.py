799dfa32afce182fd2be940d4a22080e1a314325
Ananya Majumdar <43969433+a-majumdar@users.noreply.github.com>
2022-04-21 17:53:58 +0100

    Update l4state.py

--- openflow/l4state.py
+++ openflow/l4state.py
@@ -61,7 +61,7 @@ class L4State14(app_manager.RyuApp):
                 synrst = tproto.has_flags(tcp.TCP_SYN) and tproto.has_flags(tcp.TCP_RST)
                 if flagged and not (synfin or synrst):
                     acts = psr.OFPActionOutput(ofp.OFPPC_NO_FWD)
-                if not (flow in ht):
+                if not (flow in self.ht):
                     self.add_flow(dp, 1, match, acts, msg.buffer_id)
             else:
                 if (other_port, dip, sip, dport, sport) in ht and not (flow in ht):
