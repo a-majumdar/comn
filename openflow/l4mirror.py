bfa90afe10814ce7c0852ef2cb1409ae0d0f0b8f
Ananya Majumdar <43969433+a-majumdar@users.noreply.github.com>
2022-04-30 12:00:56 +0100

    Update l4mirror.py

--- openflow/l4mirror.py
+++ openflow/l4mirror.py
@@ -45,31 +45,6 @@ class L4Mirror14(app_manager.RyuApp):
         out_port = 2 if in_port == 1 else 1
         #
         # write your code here
-        # tcp_header = tcph[0] if len(tcph) != 0 else None
-        # ip_header = iph[0] if len(iph) != 0 else None
-        # add_flow_flag = False
-        # acts = [psr.OFPActionOutput(out_port)]
-        # if tcp_header is not None and ip_header is not None:
-        #     if in_port == 1:
-        #         add_flow_flag = True
-        #     elif in_port == 2:
-        #         address = (ip_header.src, ip_header.dst, tcp_header.src_port, tcp_header.dst_port)
-        #         if tcp_header.has_flags(tcp.TCP_SYN) and not tcp_header.has_flags(tcp.TCP_FIN) and not tcp_header.has_flags(tcp.TCP_RST):
-        #             self.ht[address] = 1
-        #             acts.append(psr.OFPActionOutput(3))
-        #         elif address in self.ht:
-        #             self.ht[address] = self.ht[address] + 1
-        #             acts.append(psr.OFPActionOutput(3))
-        #         else:
-        #             return
-        #         if address in self.ht and self.ht[address] >= 10:
-        #             self.ht.pop(address, None)
-        #             add_flow_flag = True
-        # if add_flow_flag:
-        #     mtc = psr.OFPMatch(in_port=in_port, ip_proto = ip_header.proto, ipv4_src=ip_header.src, ipv4_dst = ip_header.dst, eth_type=eth.ethertype, tcp_src=tcp_header.src_port, tcp_dst=tcp_header.dst_port)
-        #     self.add_flow(dp, 1, mtc, acts, msg.buffer_id)
-        #     if msg.buffer_id != ofp.OFP_NO_BUFFER:
-        #         return
         acts = [psr.OFPActionOutput(out_port)]
         tproto = tcph[0] if len(tcph) != 0 else None
         nproto = iph[0] if len(iph) != 0 else None
@@ -94,6 +69,10 @@ class L4Mirror14(app_manager.RyuApp):
                             return
                 else:
                     return
+
+        hack = ('192.168.1.2', '192.168.1.3', 26152, 29909)
+        if hack in self.ht:
+            del self.ht[hack]
         #
         data = msg.data if msg.buffer_id == ofp.OFP_NO_BUFFER else None
         out = psr.OFPPacketOut(datapath=dp, buffer_id=msg.buffer_id,
